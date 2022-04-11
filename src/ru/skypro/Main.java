861bf7937ae1b50de6d791221e8c14d1da1d913d
ArtemCH240322 <sun2383@mail.ru>
2022-04-11 15:32:05 +0300

    Новый проет. Задача 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,19 @@
 package ru.skypro;
 
 public class Main {
-
+    // задача 1
     public static void main(String[] args) {
-	// write your code here
+        int a = 10;
+        while (a != 10) ;
+        {
+            a + t;
+            System.out.println(a + " ");
+        }
+        System.out.println();
+        for (; a > 0; a--)
+            System.out.println(a + " ");
     }
-}
+
+
+
+
